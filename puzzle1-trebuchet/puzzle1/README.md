b72d57a9feee9cd7b1afbac3b7eee35d0b835531
Xelwyk <martinrpm9000@gmail.com>
2023-12-01 23:31:32 +0100

    2nd round of refactor

---
+++ puzzle1-trebuchet/puzzle1/README.md
@@ -0,0 +1,4 @@
+usage:
+
+`./puzzle1 <path_to_file> [include_words]`
+- [include_words] - includes numbers as words when parsing lines
--- puzzle1-trebuchet/puzzle1/src/main.rs
+++ puzzle1-trebuchet/puzzle1/src/main.rs
@@ -2,44 +2,24 @@ use core::panic;
 use std::env;
 use std::fs;
 
-fn final_calibration_value(file_content: &String) -> u32 {
-    file_content.lines().map(|x: &str| get_line_calibration_value(x)).sum()
+fn final_calibration_value(file_content: &String, include_words: bool) -> u32 {
+    file_content.lines().map(|x: &str| get_line_calibration_value(x, include_words)).sum()
 }
 
-fn get_line_calibration_value(line: &str) -> u32 {
-    let mut first_number = get_first_number(line, false).to_string();
-    let last_number = get_first_number(line, true).to_string();
+fn get_line_calibration_value(line: &str, include_words: bool) -> u32 {
+    let mut first_number = get_first_number(line, false, include_words).to_string();
+    let last_number = get_first_number(line, true, include_words).to_string();
     first_number.push_str(&last_number);
     let retval = first_number.parse().expect("cannot parse line calibration");
     retval
 }
 
-fn get_first_number(input_line: &str, reverse_traverse: bool) -> char {
-    let mut line = input_line.to_lowercase();
-    if reverse_traverse {
-        line = line.chars().rev().collect::<String>();
-    }
+fn get_first_word_number(line: &str, numbers: &[String; 9]) -> (char, usize) {
     let window_size = if line.len() < 5 { line.len() } else { 5 };
     let mut window_start = 0;
     let mut window_end = window_start+window_size;
     let mut retval = '\0';
-
-    let mut numbers = [
-        "one".to_string(),
-        "two".to_string(),
-        "three".to_string(),
-        "four".to_string(),
-        "five".to_string(),
-        "six".to_string(),
-        "seven".to_string(),
-        "eight".to_string(),
-        "nine".to_string(),
-    ];
-
-    if reverse_traverse {
-        numbers = numbers.map(|x| x.chars().rev().collect());
-    }
-
+    
     while window_end <= line.len() {
         for (number, word) in numbers.iter().enumerate() {
             match &line[window_start..window_end].find(word) {
@@ -60,14 +40,49 @@ fn get_first_number(input_line: &str, reverse_traverse: bool) -> char {
         window_end += 1;
     }
 
-    //window_start is used as search limit for following for loop, if we didn't
+    //index of found word is used as search limit for numeric search pass, if we didn't
     //find any word we set limit to the end of the line
     if !retval.is_numeric() {
-        window_start = window_end;
+        window_start = line.len();
+    }
+    (retval, window_start)
+}
+
+fn get_first_number(input_line: &str, reverse_traverse: bool, include_words: bool) -> char {
+    let mut line = input_line.to_lowercase();
+    let mut retval = '\0';
+    let mut search_limit = line.len();
+    let mut numbers = [
+        "one".to_string(),
+        "two".to_string(),
+        "three".to_string(),
+        "four".to_string(),
+        "five".to_string(),
+        "six".to_string(),
+        "seven".to_string(),
+        "eight".to_string(),
+        "nine".to_string(),
+    ];
+        
+    if reverse_traverse {
+        line = line.chars().rev().collect::<String>();
+        numbers = numbers.map(|x| x.chars().rev().collect());
     }
 
+    //removing mutability
+    let line = line;
+    let numbers = numbers;
+
+    if include_words {
+        (retval, search_limit) = get_first_word_number(line.as_str(), &numbers);
+    }
+
+    //removing mutability
+    let retval = retval;
+    let search_limit = search_limit;
+        
     for (i, c) in line.chars().enumerate() {
-        if i >= window_start {
+        if i >= search_limit {
             break;
         }
         if c.is_numeric() {
@@ -83,12 +98,16 @@ fn get_first_number(input_line: &str, reverse_traverse: bool) -> char {
 }
 
 fn main() {
+    let mut include_words = false;
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("no file provided");
     }
+    if args.len() >= 3 && args[2].eq("-w") {
+        include_words = true;
+    }
 
     let file_path = &args[1];
     let content = fs::read_to_string(file_path).expect("invalid file path");
-    print!("{}", final_calibration_value(&content));
+    print!("{}", final_calibration_value(&content, include_words));
 }
