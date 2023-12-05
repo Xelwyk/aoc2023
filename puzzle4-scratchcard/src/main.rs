305aa8c75a99300d9a508ab413776130da35676a
Xelwyk <martinrpm9000@gmail.com>
2023-12-05 23:12:41 +0100

    4th puzzle part2 solved, but it takes ~1.5GB of RAM to solve it

--- puzzle4-scratchcard/src/main.rs
+++ puzzle4-scratchcard/src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs::File, io::Read};
-use std::env;
+use std::{env, vec};
 
 fn calculate_total_points(content: String) -> u32 {
     let map = content.lines()
@@ -23,6 +23,55 @@ fn calculate_total_points(content: String) -> u32 {
     ).sum()
 }
 
+fn calculate_total_scratchcards(content: String) -> u32 {
+    let map = content.lines()
+        .map(|line| line.replace("  ", " ").split(&[':', '|'][..])
+            .collect::<Vec<&str>>()
+            .iter()
+            .filter(|block| !block.trim().contains("Card"))
+            .collect::<Vec<&&str>>().iter()
+            .map(|nums| nums.trim().split(' ')
+                .collect::<Vec<&str>>()
+                .iter()
+                .map(|num| num.parse::<u32>().expect("cannot parse"))
+                .collect::<Vec<u32>>()
+            ).collect::<Vec<Vec<u32>>>()
+        ).collect::<Vec<Vec<Vec<u32>>>>();
+
+    let mut ubermap: Vec<Vec<Vec<Vec<u32>>>> = vec![];
+    for ele in map {
+        let mut wrap:Vec<Vec<Vec<u32>>> = vec![];
+        wrap.push(ele);
+        ubermap.push(wrap);
+    }
+
+    let mut line = 0;
+    while line < ubermap.len() {
+        let mut instance = 0;
+        while instance < ubermap[line].len() {   
+            println!("line:{} ins:{} - insts:{}",line,instance, ubermap[line].len());
+            let mut count = ubermap[line][instance][1].iter().filter(|num| ubermap[line][instance][0].contains(num)).collect::<Vec<&u32>>().len();
+            let mut j = 1;
+            while count > 0 {
+                //println!("count:{}",count);
+                let clone = ubermap[line+j][0].clone();
+                ubermap[line+j].push(clone);
+                count -= 1;
+                j += 1;
+            }
+            instance += 1;
+        }
+        line += 1;
+    }
+    for ele in &ubermap {
+        println!("{} - {:?}", ele.len(),ele[0]);
+        println!("------------------------");
+    }
+    return ubermap.iter().map(|e| e.len() as u32).sum()
+    //return 0
+    
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -35,5 +84,8 @@ fn main() {
         .read_to_string(&mut content)
         .expect("failed reading file content");
 
-    println!("{}", calculate_total_points(content));
+    //TODO: read oper from cmd arg
+    //println!("{}", calculate_total_points(content));
+    println!("{}", calculate_total_scratchcards(content));
+
 }
